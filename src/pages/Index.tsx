206e2cca9845e0a4d528b27a3d63f89e51ce7615
Mervin De Castro <mervindecastro@Mac-mini.local>
2026-01-06 11:26:11 +0800

    Remove testimonial and footer sections

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,12 @@
 import { Header } from "@/components/Header";
 import { ServicesGrid } from "@/components/ServicesGrid";
-import { Testimonial } from "@/components/Testimonial";
-import { BottomCTA } from "@/components/BottomCTA";
+
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
       <ServicesGrid />
-      <Testimonial />
-      <BottomCTA />
     </div>
   );
 };
